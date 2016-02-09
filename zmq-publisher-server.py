52c3e66c9387b9d284eadbf7071395d436700464
Benjamin M. Brown <benjaminbb@gmail.com>
2016-02-08 21:13:35 -0600

    adding subscriber client

--- zmq-publisher-server.py
+++ zmq-publisher-server.py
@@ -18,8 +18,10 @@ socket.bind("tcp://*:%s" % port)
 #filter on topics
 
 while True:
-	topic = random.randrange(999,1020)
-	messagedata = random.randrange(1,100)
+	# topic is zipcode, default is NYC, 10001
+	topic = random.randrange(9999,10005)
+	#our mock data is weather temps
+	messagedata = random.randrange(1,215)-80;
 	print "%d %d" % (topic, messagedata)
 	socket.send("%d %d" % (topic, messagedata))
 	time.sleep(1)
---
+++ zmq-subscriber-client.py
@@ -0,0 +1,36 @@
+import sys
+import zmq
+
+port = "5556"
+if len(sys.argv) > 1:
+	port = sys.argv[1]
+	int(port)
+
+if len(sys.argv) > 2:
+	port1 = sys.argv[2]
+	int(port1)
+
+#open socket to tlak to publisher server
+context = zmq.Context()
+socket = context.socket(zmq.SUB)
+
+print "Getting updates from server..."
+socket.connect("tcp://localhost:%s" % port)
+
+if len(sys.argv) > 2:
+	socket.connect("tcp://localhost:%s" % port1)
+
+
+#filter messages based on topic
+topicfilter = "10001"
+socket.setsockopt(zmq.SUBSCRIBE, topicfilter)
+
+#show some updates
+total_val = 0
+for update_nbr in range(1s5):
+	string = socket.recv()
+	topic,messagedata = string.split()
+	total_val += int(messagedata)
+	print topic, messagedata
+
+print "Average temp for zip '%s' was %dF" % (topicfilter, toval_val/update_nbr)
